1e900bccdc3456e68b79be28bc26ad501c3b7e1d
romersonrsantana <romersonrsantana@outlook.com>
2023-10-09 05:06:04 -0300

    Revisão
    Formulários;

--- ex0003/index.php
+++ ex0003/index.php
@@ -5,14 +5,6 @@
     <meta name="viewport" content= "width=device-width, initial-scale=1.0">
     <title>Teste Strings</title>
 </head>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 <body>
     <?php 
         $name='gustavo';
