3c2cb85284501025e28f48197be29fa44eb86d4b
romersonrsantana <romersonrsantana@outlook.com>
2023-10-10 05:21:49 -0300

    Interação php;
    Primeiras interações com php, html , js. Louvado seja Nosso Senhor Jesus Cristo!

---
+++ ex004/cad.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset = "UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processamento de dados</title>
+</head>
+<body>
+    <header>
+        <h1>Processamento de dados</h1>
+        <?php 
+        $nome = $_GET["nome"];
+        $sobrenome = $_GET["sobrenome"];
+
+        echo "<p>É um prazer te conhecer $nome $sobrenome! Este é meu site!</p>"
+        ?>
+
+        <a href="javascript:history.go(-1)">Voltar a página anterior</a>
+    </header>
+</body>
