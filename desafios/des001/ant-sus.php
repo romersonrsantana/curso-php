f4ded65dbb0b5ffcc5d078c8226ff0e04ac9d5db
romersonrsantana <romersonrsantana@outlook.com>
2023-10-15 15:19:11 -0300

    desafio
    primeira onda;

---
+++ desafios/des001/ant-sus.php
@@ -0,0 +1,20 @@
+<!DOCTYPE hmtl>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>php-dados</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <main>
+        <?php
+            $numero = $_GET["numero"];
+            $antess = $numero - 1;
+            $sucess = $numero + 1;
+            echo "<p>O número escolhido foi: <strong>$numero</strong></p>";
+            echo "<p>Seu antecessor é: <strong>$antess</strong></p>";
+            echo "<p>Seu sucessor é: <strong>$sucess</strong></p>";
+        ?>
+        <a href="index.html">voltar</a>
+    </main>
+</html>
