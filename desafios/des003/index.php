0d812f05afbb19f6c0030bb7b4c84f13f2c64b55
romersonrsantana <romersonrsantana@outlook.com>
2023-10-19 18:46:52 -0300

    exercicio

--- desafios/des003/index.php
+++ desafios/des003/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <meta charset="UTF-8">
-    <meta name="viewport" content=" width=device-width, initial sacale=1.0">
-    <TItle>Coversor</TItle>
-
-
-
+    <meta name="viewport" content=" width=device-width, initial scale=1.0">
+    <title>Coversor</title>
     <body>
+        <h1>Seu dinheiro convertido</h1>
+        <?php 
+        
+        ?>
         
     </body>
         
