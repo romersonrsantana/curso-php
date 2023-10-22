fb969a2d98bfc802b2fb43cf2aa19021251cfd6e
romersonrsantana <romersonrsantana@outlook.com>
2023-10-22 07:48:24 -0300

    exercício-3
    correção

--- desafios/des003-corrigido/conversao.php
+++ desafios/des003-corrigido/conversao.php
@@ -1,19 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html lan = "pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dinheiro convertido</title>
+        <link rel="stylesheet" href="conver-estil/style.css">
+    </head>
+    <body>
+        <main>
+            <?php
 
-$cotacao = 5.17;
+            /*$cotacao = 5.17;
 
-$real = 1000;
+            $real = 1000;
 
-$dolar = $real / $cotacao;
+            $dolar = $real / $cotacao;*/
 
-//echo "Seu dinheiro convertido é $dolar";
+            //echo "Seu dinheiro convertido é $dolar";
 
-//resposta completa
+            //resposta completa
 
-//echo "Seus R\$" . number_format($real, 2, ",", ".") . "equivalem a US\$" . number_format($dolar, 2, "," , ".");
+            //echo "Seus R\$" . number_format($real, 2, ",", ".") . "equivalem a US\$" . number_format          ($dolar, 2, "," , ".");
 
-$padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+            $real = $_GET["dinheiro"];
+            $dolar = $real / 5.17;
 
-echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a " . numfmt_format_currency ($padrao, $dolar, "USD");
+            $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
-?>
+            echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a " .  numfmt_format_currency ($padrao, $dolar, "USD");
+        ?>
+        <p><a href="index.html">Voltar</a></p>
+        </main>
+    </body>
+
+</html>
+
+
+
+
+
+
