f991fcd4ac032e83bfb9b6042a58912340ab0eaa
romersonrsantana <romersonrsantana@outlook.com>
2023-10-25 06:05:33 -0300

    ondas variáveis
    conhecendo

--- ex005/superglobais.php
+++ ex005/superglobais.php
@@ -9,8 +9,20 @@
         <main>
             <pre>
                 <?php
-                    echo "<h1>Super Global GET</h1>";
+
+                    setcookie("Dia-da-Semana", "SEGUNDA", time() + 3600); //Estou com um cookie que é o "Dia-da-semana" que está com um valor que é "segunda"
+
+                    echo "<h1>Super Global _GET</h1>";
                     var_dump($_GET); //http://127.0.0.1/curso-php/ex005/superglobais.php?nome=Guanabara&idade=45 ==> passando valores pela url;'
+
+                    echo "<h1>Super global _POST </h1>";
+                    var_dump($_POST);
+
+                    echo "<h1> Super global _REQUEST</h1>";
+                    var_dump($_REQUEST);
+
+                    echo "<h1> Super Global _COOKIE";
+                    var_dump($_COOKIE);
                 
                 ?>      
             </pre>
