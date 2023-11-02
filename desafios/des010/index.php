52a207fdec03a2e86be621b814057fe31da92049
romersonrsantana <romersonrsantana@outlook.com>
2023-11-01 22:52:17 -0300

    css

--- desafios/des010/index.php
+++ desafios/des010/index.php
@@ -4,6 +4,7 @@
         <meta charset="UTF8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Desafio 10</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <main>
@@ -15,15 +16,19 @@
             
             ?>
             <form action="<?=$_SERVER['PHP_SELF']?>" method="$_GET">
+                 <header>
+                    <h1>Portal do Aluno</h1>   
+                    <p>média escolar</p>
+                 </header>
                 <ul>
                     <li><label for="nota1">1ª Nota</label></li>
-                    <li><input type="number" name="nota1" id="nota1" value="<?=$nota1?>"></li>
-                    <li><label for="peso1">Peso 1</label></li>
-                    <li><input type="number" name="peso1" id="peso1" value="<?=$peso1?>"></li>
+                    <li class="n1"><input type="number" name="nota1" id="nota1" value="<?=$nota1?>"></li>
+                    <li ><label for="peso1">Peso 1</label></li>
+                    <li class="p1"><input type="number" name="peso1" id="peso1" value="<?=$peso1?>"></li>
                     <li><label for="nota2">2ª Nota</label></li>
-                    <li><input type="number" name="nota2" id="nota2" value="<?=$nota2?>"></li>
+                    <li class="n2"><input type="number" name="nota2" id="nota2" value="<?=$nota2?>"></li>
                     <li><label for="peso2">Peso 2</label></li>
-                    <li><input type="number" name="peso2" id="peso2" value="<?=$peso2?>"></li>
+                    <li class="p2"><input type="number" name="peso2" id="peso2" value="<?=$peso2?>"></li>
                     <li><input type="submit" value="Calcular"></li>
                 </ul>
             </form>
@@ -34,8 +39,8 @@
         ?>
         <section>
             <?php 
-                echo "<p>Sua média simples é $medsimp</p>";
-                echo "<p>Sua média ponderada é " . number_format($medpond, 3, "," , ".") . "</p>";
+                echo "<p>Média simples é $medsimp</p>";
+                echo "<p>Média ponderada é " . number_format($medpond, 3, "," , ".") . "</p>";
             ?>
         </section>
         
