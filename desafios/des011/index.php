23bfbb189ec22a7499e8c3e667e4cc049a1e3692
romersonrsantana <romersonsantana@hotmail.com.br>
2023-11-12 19:32:36 -0300

    Aperfeiçoando a navegação
    Descobrindo o elixir do meu sonho.
    
    Louvado seja Nosso Senhor Jesus Cristo. Para sempre seja Louvado.

--- desafios/des011/index.php
+++ desafios/des011/index.php
@@ -4,6 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Desafio 10</title>
+        <link rel="stylesheet" media="all" href="style.css" > <!--Para todos os tipos de midia-->
+        <link rel="stylesheet" media="only screen and (min-width: 320px) and (max-width: 991px" href="cell-phone.css"> <!--Tentar criar uma regra para portrait e landscape dentro da olha de estilo -->
     </head>
     <body>
         <?php 
@@ -20,21 +22,21 @@
             <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
 
                 <ul>
+                    </li><?php print "<p>Estamos no ano de $data</p>;"?></li>
+
                     <li>
                         <label for="data">Seu ano de nascimento </label>
-                        <input type="number" name="data" id="data" value="<?=$nasciment?>">
-                    </li>
+                        <input type="number" name="data" id="data" min="<?=$data-126?>" max="<?=$data-1?>" value="<?=$nasciment?>">
                     <li>
-                        <label for="anoAtual">Digite um ano, considerando que estamos em <?php print "$data";?></label>
-                        <input type="number" name="anoAtual" id="anoAtual">
-                    </li>
-                    <li>
-                        <input type="submit" value="Calcular">
-                    </li>
+                    <li><input type="submit" value="Calcular"></li>
                 </ul>
-            </form>
+            </form>   
+                
             <?php 
-               
+               $idade= ($data - $nasciment );
+               $meses= ($idade*12);
+
+               print "<p>Você tem $idade ano(s) de idade</p> <p>Considerando seu ano de nascimento e sua idade posso te dizer que você já viveu por $meses meses !! O equivalente a " . number_format($idade*360, 0, ',','.')  . " dias</p><p>As contas não param por aqui meu amigo(a) você já possui " . number_format(($idade*360)*24,2,',','.') . " horas de experiência no simples ato de viver. Como tem sido gasto todas essas <strong>" . number_format(($idade*360)*24, 0, ',' , '.') . " horas?</strong> <p>Não sei em que ponto da vida você se encontra nesse instante, o que posso te dizer é que de certa forma você já acumulou mais de " . number_format((($idade*360)*24)*60,2, ',','.') . " minutos de vida, o que nos permite compreender que cada minuto tem um valor e um peso, por essa razão espero que você entenda a importância de aproveitar cada instânte para lutar pelo seu propósito, que todo tempo gastado até aqui, sirva de combustível, de experiência para todas as lutas que  ainda estão por vim. E não se esqueça de agradecer a Deus por todo esse tempo que já tem e de Coloca-lo à frente de todos os seus projetos!!Amém. </p>"
             ?>
 
         </main>
