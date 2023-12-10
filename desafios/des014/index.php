bfb383a5ce39ddb9ab4d4cf02cf51900103f321b
romersonrsantana <romersonsantana@hotmail.com.br>
2023-12-10 08:04:08 -0300

    Create index.php

---
+++ desafios/des014/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset = "UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Caixa eletrônico</title> 
+    </head>
+    <body>
+        <main>
+            <?php
+            
+            $dinheiro = $_GET["valor"] ?? $dinheiro;
+            
+            ?>
+            <form action="" method="get">
+                <label for="valor">Digite um valor para saque</label>
+                <input type="number" name="valor" id="valor" min="5" max="1500" value="<?=$dinheiro?>">
+                <input type="submit" value="sacar">
+            </form>
+        </main>
+        <section>
+            <h1>Saque</h1>
+            <?php 
+                echo"<p>O valor digitado corresponde há </p>" . $dinheiro . " reais";
+
+                $calc1 = $dinheiro/100;
+                $calc2 = ($dinheiro%100)/50;
+                $calc3 = (($dinheiro%100)%50)/10;
+                $calc4 = ((($dinheiro%100)%50)%10)/5;
+
+            ?>
+            <ul>
+                <li>Notas de R$ 100,00: <?=(int)$calc1?> </li>
+                <li>Notas de R$ 50,00:  <?=(int)$calc2?> </li>
+                <li>Notas de R$ 10,00:  <?=(int)$calc3?> </li>
+                <li>Notas de R$ 5,00:   <?=(int)$calc4?> </li>
+            </ul>
+            
+
+        </section>
+    </body>
+</html>
